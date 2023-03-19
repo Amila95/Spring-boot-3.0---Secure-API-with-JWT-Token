8b05ae0f9c47610d6a6249a5280730b8033924e4
AWickramarathna <awickramarathna@typefi.com>
2023-03-19 16:00:54 +0530

    init commit

---
+++ src/main/java/com/Auth/JwtAuth/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.Auth.JwtAuth.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/Auth/JwtAuth/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.Auth.JwtAuth.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
